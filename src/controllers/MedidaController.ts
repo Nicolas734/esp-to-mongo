dae167373a0d0da9b826831942543c38455b18ce
Nicolas <nichollaslimma734@gmail.com>
2023-04-18 20:29:02 -0300

    finalizado conversão do codigo para typescript

--- src/controllers/MedidaController.ts
+++ src/controllers/MedidaController.ts
@@ -1,5 +1,24 @@
-class MedidaController{
-    public async cadastrar(req, res){
+import { buscarMedidas, inserirMedida } from "../services/medidaService";
 
+
+class MedidaController{
+    public async buscar(req, res){
+        try {
+            const medidas = await buscarMedidas();
+            res.json(medidas);
+        } catch (error) {
+            res.json(error);
+        }
+    }
+
+    public async cadastrar (req, res){
+        try{
+            const dados = await inserirMedida(req.body);
+            res.json(dados);
+        } catch (error) {
+            res.json(error)
+        }
     }
 }
+
+export default new MedidaController();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,31 +1,13 @@
 import { Router } from "express";
 import { buscarMedidas, inserirMedida } from "../services/medidaService";
+import MedidaController from "../controllers/MedidaController";
 
 
 
 const routes = Router();
 
-const buscar = async (req, res) => {
-    try {
-        const medidas = await buscarMedidas();
-        res.json(medidas);
-    } catch (error) {
-        res.json(error);
-    }
-}
 
-
-const cadastrar = async (req, res) => {
-    try{
-        const dados = await inserirMedida(req.body);
-        res.json(dados);
-    } catch (error) {
-        res.json(error)
-    }
-}
-
-
-routes.get('/buscar', buscar);
-routes.post('/cadastrar', cadastrar);
+routes.get('/buscar', MedidaController.buscar);
+routes.post('/cadastrar', MedidaController.cadastrar);
 
 export default routes;
