f2e5b3ca7c21978af032c13a86ae332e4e4b989a
Nicolas Lima <nichollaslimma734@gmail.com>
2023-04-22 14:36:45 -0300

    adicionado vercel

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import { connect, medidaCollection } from './config/db'; 
-import routes from "./routes";
+// import routes from "./routes";
+
 
 
 dotenv.config();
@@ -14,7 +15,47 @@ connect();
 
 app.use(express.json());
 app.use(cors());
-app.use(routes);
+// app.use(routes);
+
+
+const buscar = async (req, res) => {
+    try {
+        const results = await medidaCollection.find().toArray();
+        res.json(results);
+    } catch (error) {
+        res.json(error);
+    }
+}
+
+
+const cadastrar = async (req, res) => {
+    try{
+        const dados = req.body;
+        const result = await medidaCollection.insertOne(dados);
+        res.json(result);
+    } catch (error) {
+        console.log(error)
+        res.json(error)
+    }
+}
+
+
+const cadastrarMultiplo = async (req, res) => {
+    try{
+        const dados = req.body;
+        const result = await medidaCollection.insertMany(dados);
+        res.json(result);
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+
+app.get('/buscar', buscar);
+app.post('/cadastrar', cadastrar);
+app.post('/cadastrar-multiplos', cadastrarMultiplo);
+
+
 
 
 
